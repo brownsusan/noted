7ddd4cada60f3d2aaa981b76cfb5572bd68dfb5c
Susan Brown <brownsusan@fullsail.edu>
2013-10-10 08:51:51 -0400

    Added the update feature.

--- controllers/action_note_update.php
+++ controllers/action_note_update.php
@@ -1,5 +1,5 @@
 <?php
-
+//Needs valdation
 $note_id = $_POST['note_id'];
 $title = $_POST['title'];
 $body = $_POST['body'];
@@ -9,5 +9,5 @@ $category = $_POST['category'];
 $note_model = new noteModel();
 //This variable stores what is returned from the getNote function ($obj)
 $note = $note_model -> updateNote($note_id, $title, $body, $category);
-
+//header does a redirect - changing the view back to notes
 header('Location: /?action=app');
