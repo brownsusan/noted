7c228011ef5349b9f0b03961970ca6c8d80c5c2b
Susan Brown <brownsusan@fullsail.edu>
2013-10-11 20:52:24 -0400

    create categories dropdown

--- controllers/note_create.php
+++ controllers/note_create.php
@@ -5,8 +5,12 @@ $viewModel = new viewModel();
 //Temp user data
 $user = array('name' => 'susie');
 
+$category_model = new categoryModel();
+$categories = $category_model -> getCategories();
+
+
 //-> is similar to dot syntax - passing the note that was returned
 $viewModel -> getView('views/template/header.php', $user);
-$viewModel -> getView('views/note_create/body.php');
+$viewModel -> getView('views/note_create/body.php', $categories);
 $viewModel -> getView('views/notes/nav.php');
 $viewModel -> getView('views/template/foot.php');
--- views/note_create/body.php
+++ views/note_create/body.php
@@ -5,7 +5,15 @@
 				<h1>Create a new note</h1>
 				<div class="create-note-header">
 					<input type="text" class="create-note-title" placeholder="Enter Note Title..." name="title" />
-					<input type="text" class="create-note-category" placeholder="Enter Note Category..." name="category" />
+					<select class="create-note-category" name="category">
+						<?php 
+						
+						foreach($data as $category){
+							echo('<option value="'.$category['categoryId'].'">'.$category['title'].'</option>');
+						}
+						
+						?>
+					</select>
 				</div>
 				<!-- END HEADER DIV -->
 					<textarea class="note-create-body" name="body" placeholder="Enter Note..."></textarea>
