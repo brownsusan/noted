dea735711dcbb0f4211af3e421c595cf388ab61f
Susan Brown <brownsusan@fullsail.edu>
2013-10-24 01:15:57 -0400

    commit

--- controllers/action_note_create.php
+++ controllers/action_note_create.php
@@ -17,7 +17,7 @@ if(
 $userId = $_SESSION['user']['userId'];
 $title = $_POST['title'];
 $body = $_POST['body'];
-$category = $_POST['category'];
+$category = $_POST['categoryTitle'];
 
 if($category == ''){
 	$category = 1;
